59b52d8da0673916bcd94d57e84ed1683e700786
Hélio Neto <85686522+netocabral@users.noreply.github.com>
2022-12-19 20:04:01 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -9,13 +9,44 @@ data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
+    var nivel: Nivel = Nivel.BASICO
+    var valor: Float = 0.0f
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    
+    fun getDuracaoTotal(): Int {
+        var duracaoTotal = 0
+        for (conteudo in conteudos) {
+            duracaoTotal += conteudo.duracao
+        }
+        return duracaoTotal
+    }
+    
+    fun estaMatriculado(usuario: Usuario): Boolean {
+        return usuario in inscritos
     }
 }
 
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 45)
+    val conteudo2 = ConteudoEducacional("Estruturas de Dados", 75)
+    val conteudo3 = ConteudoEducacional("Algoritmos Avançados")
+    val conteudos = listOf(conteudo1, conteudo2, conteudo3)
+    val formacao = Formacao("Curso de Desenvolvimento de Software", conteudos)
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+    
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    
+    println("Número de inscritos: ${formacao.inscritos.size}") // deve imprimir "Número de inscritos: 2"
+    
+    println("Duração total da formação: ${formacao.getDuracaoTotal()} minutos") // deve imprimir "Duração total da formação: 180 minutos"
+    
+    println("Usuário 1 está matriculado? ${formacao.estaMatriculado(usuario1)}") // deve imprimir "Usuário 1 está matriculado? true"
+    println("Usuário 3 está matriculado? ${formacao.estaMatriculado(Usuario())}") // deve imprimir "Usuário 3 está matriculado? false"
 }
+
